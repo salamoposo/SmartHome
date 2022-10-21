1e29f30cdf213bf49a4a7a9641e112a1ec9861fc
salahuddin <salamoposo@gmail.com>
2022-10-21 21:43:09 +0800

    update project smarthome

---
+++ app/src/main/java/com/example/smarthome/ProfileActivity.java
@@ -0,0 +1,34 @@
+package com.example.smarthome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    ImageView backButton;
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+}
